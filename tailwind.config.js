92b2dfdcccd4d760a1f5aed32195168ba6aba345
Ridoy Roy <ridoyroycse53engg@gmail.com>
2023-03-31 09:32:36 +0600

    Add daisy Ui theme & exclute full height banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,32 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#0FCFEC",
+
+          "secondary": "#19D3AE",
+
+          "accent": "#37CDBE",
+
+          "neutral": "#3A4256",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 
 }
