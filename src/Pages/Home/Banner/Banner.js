3a2bdedab26a5d92531675bb9b5483e22b802c43
Ridoy Roy <ridoyroycse53engg@gmail.com>
2023-03-31 22:31:30 +0600

    Fix banner responsive issues

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -15,7 +15,7 @@ const Banner = () => {
 
                 }}
             >
-                <img src={bannerImage} className=" w-1/2 rounded-lg shadow-2xl" alt='' />
+                <img src={bannerImage} className=" lg:w-1/2 rounded-lg shadow-2xl" alt='' />
                 <div>
                     <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
                     <p className="py-6">All doctors treat, but a good doctor lets nature heal!</p>
