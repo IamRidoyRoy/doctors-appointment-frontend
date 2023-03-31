2e3fb83884f49085ed10805847cbf69806293ae5
Ridoy Roy <ridoyroycse53engg@gmail.com>
2023-03-31 09:57:51 +0600

    Allign navbar and banner with text

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import bannerImage from '../../../assets/images/chair.png'
+import './Banner.css';
 const Banner = () => {
     return (
         // This is homepage banner 
 
         <div className="hero  bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={bannerImage} className="max-w-sm rounded-lg shadow-2xl" alt='' />
+            <div className="hero-content flex-col lg:flex-row-reverse px-12">
+                <img src={bannerImage} className=" w-1/2 rounded-lg shadow-2xl" alt='' />
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+                    <p className="py-6">All doctors treat, but a good doctor lets nature heal!</p>
+                    <button className="bannerbtn btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">Get Started</button>
                 </div>
             </div>
         </div>
