ac3e56e519b205a713485637f2a585d4bfa4e78f
Ridoy Roy <ridoyroycse53engg@gmail.com>
2023-04-03 22:17:58 +0600

    available slots with conditional rendering

--- src/Pages/AppointmentPage/AptOptions/AptOptions.js
+++ src/Pages/AppointmentPage/AptOptions/AptOptions.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const AptOptions = ({ option }) => {
-    // const { slots } = option;
-    // console.log(option)
+    const { name, slots } = option;
     return (
 
-        <div className="card w-80 bg-base-100 shadow-xl text-center ">
+        <div className="card  bg-base-100 shadow-xl text-center ">
             <div className="card-body">
-                <h2 className="card-title justify-center text-secondary">{option.name}</h2>
+                <h2 className="card-title justify-center text-secondary">{name}</h2>
+                <p>{slots.length > 0 ? slots[0] : "Try another day!"}</p>
+                <p>{slots.length} {slots.length > 1 ? "spaces available" : 'space available'}!</p>
                 <div className="card-actions justify-center">
                     <button className="btn btn-primary text-white">Book Now</button>
                 </div>
