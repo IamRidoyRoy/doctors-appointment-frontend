a6112b04a1034d4dc2aaaac22c886d41fc65f7fd
Ridoy Roy <ridoyroycse53engg@gmail.com>
2023-05-04 11:41:56 +0600

    Fetching data using react query

--- src/Pages/AppointmentPage/Appointment.js
+++ src/Pages/AppointmentPage/Appointment.js
@@ -3,6 +3,7 @@ import AptBanner from './AptBanner/AptBanner';
 import AvailableServices from './AvailableServices/AvailableServices';
 import AptOptions from './AptOptions/AptOptions';
 import BookingModal from './BookingModal/BookingModal';
+import { useQuery } from 'react-query';
 
 const Appointment = () => {
     // This state for selecting date 
@@ -13,20 +14,29 @@ const Appointment = () => {
     const [treatment, setTreatment] = useState(null);
 
     // This state for booking name , slots options 
-    const [options, setOptions] = useState([]);
-    console.log(options)
-    useEffect(() => {
-        fetch('http://localhost:5000/time-slots')
+    // const [options, setOptions] = useState([]);
+
+
+    // Fetching api data using react query 
+    const { data: slots = [] } = useQuery({
+        queryKey: ['slots'],
+        queryFn: () => fetch('http://localhost:5000/time-slots')
             .then(res => res.json())
-            .then(data => setOptions(data))
-    }, [])
+    })
+    console.log(slots)
+
+    // useEffect(() => {
+    //     fetch('http://localhost:5000/time-slots')
+    //         .then(res => res.json())
+    //         .then(data => setOptions(data))
+    // }, [])
     return (
         <div className='mx-5'>
             <AptBanner selected={selected} setSelected={setSelected}></AptBanner>
             <AvailableServices selected={selected}></AvailableServices>
             <div className='lg:mx-14 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '>
                 {
-                    options.map((option) => <AptOptions setTreatment={setTreatment} key={option._id} option={option}></AptOptions>
+                    slots.map((option) => <AptOptions setTreatment={setTreatment} key={option._id} option={option}></AptOptions>
                     )
                 }
             </div>
